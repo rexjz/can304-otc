7e38eca8affa38c8e61f6b58c0666892e3444c26
rexjz <robbort.xu@gmail.com>
2023-05-08 14:32:25 +0800

    hash chain

--- server.py
+++ server.py
@@ -1,42 +1,45 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import http
-import argparse
-import os
-
-server_store = './client_store/'
-
-parser = argparse.ArgumentParser(
-    prog='CAN 304 Demo Server',
-    description=
-    'This is the demo server used to verify One Time Cookie for can304 group project of Group 32',
-)
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
 
 
-def get_server_store():
-    if not os.path.exists(server_store):
-        os.makedirs(server_store)
-    return server_store
-
-
-def process_request(request):
-    print(request)
-
-
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/text')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write("need login".encode('utf-8'))
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
     def do_POST(self):
-        self.send_response(200)
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
 
-
-def run(server_class=HTTPServer, handler_class=RequestHandler):
-    server_address = ('', 8000)
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
 
-run()
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
